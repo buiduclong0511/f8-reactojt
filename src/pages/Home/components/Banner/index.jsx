dbfbc639b0b1fbae9dacc87fd8d01a23b2c2a871
buiduclong0511 <buiduclong0511@gmail.com>
2022-07-05 15:34:35 +0700

    Chuẩn bị giao diện cho phần Banner

---
+++ src/pages/Home/components/Banner/index.jsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames/bind';
+import { Button } from '~/components';
+import { Container } from 'reactstrap';
+import styles from './Banner.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Banner() {
+    return (
+        <div className={cx('background')}>
+            <Container fluid="lg">
+                <div className={cx('wrapper')}>
+                    <img src="images/lamp.png" alt="" className={cx('lamp-img')} />
+                    <div className={cx('promotion')}>
+                        <p className={cx('sub-title')}>Best Furniture For Your Castle....</p>
+                        <h3 className={cx('title')}>New Furniture Collection Trends in 2020</h3>
+                        <p className={cx('description')}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in
+                            phasellus non in justo.
+                        </p>
+                        <div className={cx('cta-btn')}>
+                            <Button>Shop Now</Button>
+                        </div>
+                    </div>
+                    <img src="images/sofa_promotional.png" alt="" className={cx('promotional-img')} />
+                </div>
+            </Container>
+        </div>
+    );
+}
+
+export default Banner;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,19 @@
+import classNames from 'classnames/bind';
+import { BranchesList } from '~/components';
+import Banner from './components/Banner';
+import styles from './Home.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Home() {
-    return <div>Home</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('banner-slider')}>
+                <Banner />
+            </div>
+            <BranchesList />
+        </div>
+    );
 }
 
 export default Home;
